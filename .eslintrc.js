ee50b873820b8c3ab6a38bc3e882071f5c744bf7
Kevin Enario <kevin_enario@yahoo.com>
2020-09-05 17:47:19 -0700

    Turned off no-unused-before-define rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 0,
-    '@typescript-eslint/no-non-null-assertion': 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-use-before-define': false
   },
   overrides: [
     {
