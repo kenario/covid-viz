58b8b5b68d0714f84c472de5908d0930e9017e5c
Kevin Enario <kevin_enario@yahoo.com>
2020-09-10 23:58:45 -0700

    Removed js-datepicker, no typescript support and added flatpickr.

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="date-picker-container">
+    <div id="date-picker">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import flatpickr from 'flatpickr'
+
+export default Vue.extend({
+  name: 'Date Picker',
+
+  created() {
+    flatpickr('#date-picker', {})
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
