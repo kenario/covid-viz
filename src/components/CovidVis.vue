769b2f8e4ebcb57697dedad7f8e6094fcfd3321d
Kevin Enario <kevin_enario@yahoo.com>
2020-12-08 21:41:53 -0800

    Added country code to country name conversion for geolocation processes.

--- src/components/CovidVis.vue
+++ src/components/CovidVis.vue
@@ -15,6 +15,7 @@ import CovidChart from './CovidChart.vue'
 import CovidGeneralInfo from './CovidGeneralInfo.vue'
 import CovidVisControls from './CovidVisControls.vue'
 import { geolocationEP } from '../shared/constants/geolocationEP'
+import { CountryInfo } from '../types'
 
 export default Vue.extend({
   name: 'CovidVis',
@@ -43,19 +44,27 @@ export default Vue.extend({
 
   async mounted() {
     await this.$store.dispatch('getCovidDataAllCountries')
+    this.locateUser()
+
     this.$store.commit('setSelectedCountry', this.location)
     this.$store.commit('setSelectedCovidData')
     await this.$store.dispatch('getHistoricalCountryData')
-    // this.locateUser()
   },
 
   methods: {
+    /**
+     * If geolocation is avilable then we compare the location to the country codes of the countries
+     * affected by covid and mutate the selectedCountry state with that country.
+     */
     locateUser(): void {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async (position: GeolocationPosition): Promise<void> => {
           const res = await fetch(geolocationEP(position.coords.latitude, position.coords.longitude))
           const data = await res.json()
-          this.$store.commit('setSelectedCountry', data.address.country_code)
+          const country: string = this.getAllAffectedCountries.find((countryInfo: CountryInfo): boolean =>
+            data.address.country_code.toUpperCase() === countryInfo.countryCode).name
+
+          this.$store.commit('setSelectedCountry', country)
         })
       }
     }
