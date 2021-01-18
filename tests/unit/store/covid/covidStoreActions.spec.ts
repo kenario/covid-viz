b0ad17f23c732730eff803fa13db7585257c6f39
Kevin Enario <kevin_enario@yahoo.com>
2021-01-17 16:04:52 -0800

    Added sinon for stubbing.

---
+++ tests/unit/store/covid/covidStoreActions.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import { state, actions } from '@/store/covid'
+
+const { getCovidDataAllCountries, getHistoricalCountryData } = actions
+
+describe('Covid Store actions', (): void => {
+
+})
