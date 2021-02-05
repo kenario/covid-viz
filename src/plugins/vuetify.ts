a29657a94120b015e42cd57d9bfda10497f2c092
Kevin Enario <kevin_enario@yahoo.com>
2021-02-04 19:28:03 -0800

    Added default color theme configuration.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#184F63',
+        secondary: '#CED5AE',
+        accent: '#DA7F46'
+      }
+    }
+  }
 })
