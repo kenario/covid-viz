c38897531fa4fb2621d08aab11b6bc2af1954424
Kevin Enario <kevin_enario@yahoo.com>
2021-04-10 17:21:46 -0700

    Added unit tests for rankCovidData and mapCovidTotals helper functions.

--- tests/unit/store/covid/helpers.spec.ts
+++ tests/unit/store/covid/helpers.spec.ts
@@ -9,6 +9,8 @@ import {
 import moment from 'moment'
 import { expect } from 'chai'
 import { SelectItem } from '@/types'
+import { CovidCountryData, CovidRankingData, CovidTotals } from '@/types/covid'
+import { covidStateMocks } from '../../covidMocks'
 
 describe('CovidStoreHelpers', (): void => {
   const data = {
@@ -58,6 +60,32 @@ describe('CovidStoreHelpers', (): void => {
     it('returns total values per day when ResultType is total per day', (): void => {
       const expected = [-40, -141, 3775, -3795]
       expect(determineCovidChartData(values, { name: 'Total Per Day', value: 'totalPerDay' })).to.deep.equal(expected)
+    })
+  })
+
+  describe('rankCovidData', (): void => {
+    it('returns ranking that is in descending order', (): void => {
+      const covidCountryData: CovidCountryData[] = covidStateMocks.generateCovidDataAllCountries()
+      const totals: number[] = rankCovidData(covidCountryData, 'country', 'casesPerOneMillion')
+        .map((data: CovidRankingData): number => data.total)
+      const isDescended: boolean = totals.every((current: number, index: number): boolean =>
+        index === totals.length - 1 ? true : current > totals[index + 1]
+      )
+      expect(isDescended).to.be.true
+    })
+  })
+
+  describe('mapCovidTotals', (): void => {
+    it('returns totals for cases, recovered, deaths, tests, vaccinated, and updated date', (): void => {
+      const data: CovidCountryData = covidStateMocks.generateCovidDataAllCountries()[0]
+      const covidCountryTotals: CovidTotals = mapCovidTotals(data)
+
+      expect(covidCountryTotals.cases).to.equal(data.baseData.cases)
+      expect(covidCountryTotals.recovered).to.equal(data.baseData.recovered)
+      expect(covidCountryTotals.deaths).to.equal(data.baseData.deaths)
+      expect(covidCountryTotals.tests).to.equal(data.baseData.tests)
+      expect(covidCountryTotals.vaccinated).to.equal(data.baseData.vaccinated)
+      expect(covidCountryTotals.updated).to.equal(data.baseData.updated)
     })
   })
 })
