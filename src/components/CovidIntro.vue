0b4ae2d5625b1b13903a888a226d27d341c239b4
Kevin Enario <kevin_enario@yahoo.com>
2021-08-17 22:23:28 -0700

    Added intro for vaccine data.

--- src/components/CovidIntro.vue
+++ src/components/CovidIntro.vue
@@ -19,7 +19,8 @@ export default Vue.extend({
       Welcome to CORONA Trends, a simple dashboard for tracking information related to the Novel
       Covid-19 virus or Coronavirus.  The data from this site is pulled from the disease.sh API,
       which aggregates data from multiple different sources around the world.  Granularity of some
-      of the data on this website is only currently availabe for the United States.
+      of the data on this website is only currently availabe for the United States. Vaccinated
+      data is in doses, meaning it is counting the 1st and 2nd dose separately where applicable.
     `
   })
 
