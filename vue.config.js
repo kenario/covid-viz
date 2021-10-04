d694f4b67fa1c437b74f9090a1c1dc8d62261064
Kevin Enario <kevin_enario@yahoo.com>
2021-10-03 21:36:39 -0700

    Updating prod.

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/covid-viz/' : ''
+  publicPath: process.env.NODE_ENV === 'production' ? '/covid-viz/' : '',
+  css: {
+    extract: false
+  }
 }
